b41040c3d8a4e9993fcd1062f9e4050a12b30ddc
Neo <choneo.ch@gmail.com>
2022-04-15 23:09:01 -0700

    img

--- index.js
+++ index.js
@@ -17,12 +17,15 @@ app.get('/', (req, res) => {
   res.send('hello')
 })
 
+let images = ['https://lh3.googleusercontent.com/jJsUhpTNA_9CwMePUgJZamQW1IIHQgt3Hx1of8Y8EHhKqBsjHU9xb03S79xXzqLpGCVUX243N8dxYNkKaRcc51pFQh6bwZg5F8f-Ig', 'https://lh3.googleusercontent.com/bdECg069zBNQ-8uaxdTrNMdILTElSW4tSZ0zUFSrAuCAJOmqR6TKPigPrfk-ZAN6GS7mNWjuv3qGCxx2NGJ_3Zli9FCOdDvrxh10=w600', 'https://lh3.googleusercontent.com/IS4ySvhI8jGQsg_2fNFT3127JK61umYOGkulDrgVNxkBMeXz6acFjWjK1hmMBnMHPYnUq95NvmbBtUnfso8oR9GpRx_yL8L3w5lm=w600', 'https://lh3.googleusercontent.com/7h6yA2MwfeVC_k493JXGDbVetx7hlIFEohvflEBSVKH2C-OhEXvzUZdNcaAKWEMJMDZUFaljbHi2LzZg6busnhzi6fExqsgygAuEEA=w600'];
 let persons = {}
 io.on('connection', (socket) => {
   // On connect, create a new entry for the person
   let uuid = randomUUID()
   let color = Math.floor(Math.random() * 255)
-  const metadata = { uuid, color }
+  let image = Math.floor(Math.random() * 3)
+  let currImage = images[image]; 
+  const metadata = { uuid, color, currImage }
   persons[socket.id] = metadata
 
   console.log(`Connected: ${socket.id}`)
