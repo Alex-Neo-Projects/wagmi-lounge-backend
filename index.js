a5b0ab3c510e26ae85b9de93dd4b3435f84fdf7c
Neo <choneo.ch@gmail.com>
2022-04-15 23:02:35 -0700

    test

--- index.js
+++ index.js
@@ -17,20 +17,15 @@ app.get('/', (req, res) => {
   res.send('hello')
 })
 
-let images = ['https://lh3.googleusercontent.com/jJsUhpTNA_9CwMePUgJZamQW1IIHQgt3Hx1of8Y8EHhKqBsjHU9xb03S79xXzqLpGCVUX243N8dxYNkKaRcc51pFQh6bwZg5F8f-Ig', 'https://lh3.googleusercontent.com/bdECg069zBNQ-8uaxdTrNMdILTElSW4tSZ0zUFSrAuCAJOmqR6TKPigPrfk-ZAN6GS7mNWjuv3qGCxx2NGJ_3Zli9FCOdDvrxh10=w600', 'https://lh3.googleusercontent.com/IS4ySvhI8jGQsg_2fNFT3127JK61umYOGkulDrgVNxkBMeXz6acFjWjK1hmMBnMHPYnUq95NvmbBtUnfso8oR9GpRx_yL8L3w5lm=w600', 'https://lh3.googleusercontent.com/7h6yA2MwfeVC_k493JXGDbVetx7hlIFEohvflEBSVKH2C-OhEXvzUZdNcaAKWEMJMDZUFaljbHi2LzZg6busnhzi6fExqsgygAuEEA=w600'];
-
 let persons = {}
 io.on('connection', (socket) => {
-  console.log('connected!!!')
-
   // On connect, create a new entry for the person
   let uuid = randomUUID()
   let color = Math.floor(Math.random() * 255)
-  let image = Math.floor(Math.random() * 3)
-  
-  var currImage = images[image]; 
-  const metadata = { uuid, color, currImage }
+  const metadata = { uuid, color }
   persons[socket.id] = metadata
+
+  console.log(`Connected: ${socket.id}`)
 
   // Get the cursor data and publishes to other clients
   socket.on('position', (data) => {
@@ -41,9 +36,17 @@ io.on('connection', (socket) => {
     io.sockets.emit('position', JSON.stringify(persons))
   })
 
+  socket.on('chat', (data) => {
+    const message = JSON.parse(data)
+
+    persons[socket.id] = { ...persons[socket.id], chat: message.chat }
+
+    io.sockets.emit('chat', JSON.stringify(persons))
+  })
+
   // Person disconnects
   socket.on('disconnect', (data) => {
-    console.log('disconnected!')
+    console.log(`Disconnected: ${socket.id}`)
 
     delete persons[socket.id]
 
